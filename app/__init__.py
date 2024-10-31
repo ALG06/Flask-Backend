10df3a0110f95db551af727351f51bf28a869510
DanielBrMz <a01254805@tec.mx>
2024-10-31 12:00:00 -0600

    models

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from config import Config
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    from app.routes import auth_bp
+    app.register_blueprint(auth_bp)
+
+    return app
