dd053d842d39b0e31536de035d0304183343eca1
Andrés Martínez <autummata@gmail.com>
2024-11-21 00:53:25 -0600

    Fix: Datetime now uses proper method

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
